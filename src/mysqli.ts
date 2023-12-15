2ea5ddc9e0dbd94439b2a0eca45bc11b6609abf3
Josh Tomlinson <jtomlinson@cpmstar.com>
2023-12-14 17:42:45 -0800

    Mysql Interface: Interface with mysql database using http crud.

--- src/mysqli.ts
+++ src/mysqli.ts
@@ -1,12 +1,12 @@
 import mysql from "mysql2"
 import _ from "lodash"
 
-interface config {
+export interface MysqlConfig {
     host: string,
     user: string,
     password: string
 }
-interface getInfo {
+export interface GetInfo {
     database: string,
     table: string,
     id?: any,
@@ -14,18 +14,18 @@ interface getInfo {
     properties?: string | string[],
     limit?: string | number
 }
-interface postInfo {
+export interface PostInfo {
     database: string,
     table: string,
     properties: any
 }
-interface putInfo {
+export interface PutInfo {
     database: string,
     table: string,
     id: any,
     properties: any
 }
-interface deleteInfo {
+export interface DeleteInfo {
     database: string,
     table: string,
     id: any
@@ -34,11 +34,11 @@ interface deleteInfo {
 export default class MysqlInterface {
     private server: any = undefined;
 
-    constructor( private config: config ) {
-        console.log("Mysql Interface initialized successfully!");
+    constructor( private config: MysqlConfig, private debug: boolean = false ) {
+        if ( debug ) { console.log("Mysql Interface initialized successfully!"); }
     }
 
-    private async query( query: string ) {
+    public async query( query: string ) {
         let self = this;
         return new Promise(function( resolve, reject ) {
             self.server.query(query, function( error: any, result: any, fields: any ) {
@@ -50,7 +50,6 @@ export default class MysqlInterface {
             });
         });
     }
-
     public async connect(): Promise<void> {
         let self = this;
         return new Promise(function( resolve, reject ) {
@@ -75,11 +74,12 @@ export default class MysqlInterface {
                         resolve();
                     }
                 });
+            } else {
+                resolve();
             }
-            
         });
     }
-    public async delete({ database, table, id }: deleteInfo ): Promise<void> {
+    public async delete({ database, table, id }: DeleteInfo ): Promise<void> {
         let self = this;
         id = "where " + Object.keys( id ).map(key => { 
             return key + " = " + JSON.stringify( id[key] );
@@ -87,7 +87,7 @@ export default class MysqlInterface {
         let query = "delete from " + database + "." + table + " " + id + ";";
         await self.query( query );
     }
-    public async put({ database, table, id, properties }: putInfo): Promise<void> {
+    public async put({ database, table, id, properties }: PutInfo): Promise<void> {
         let self = this;
         properties = Object.keys( properties ).map(key => {
             return key + " = " + JSON.stringify( properties[key] );
@@ -98,13 +98,13 @@ export default class MysqlInterface {
         let query = "update " + database + "." + table + " set " + properties + " where " + id + ";";
         await self.query( query );
     }
-    public async post({ database, table, properties }: postInfo): Promise<number> {
+    public async post({ database, table, properties }: PostInfo): Promise<number> {
         let self = this;
         let keys = Object.keys( properties ).join(", ");
         let values = Object.values( properties ).map(property => { return JSON.stringify( property ); }).join(", ");
         let query = "insert into " + database + "." + table + " ( " + keys + " ) values( " + values + " );";
         let result: any = await self.query( query );
-        let id = result.insertId;
+        let id = result?.insertId;
         return id;
     }
     public async get({
@@ -114,7 +114,7 @@ export default class MysqlInterface {
         limit = "",
         where = "",
         properties = "*"
-    }: getInfo): Promise<any> {
+    }: GetInfo): Promise<any> {
         let self = this;
         if (
             _.isEmpty( id ) == false && 
@@ -152,4 +152,161 @@ export default class MysqlInterface {
             return result;
         }
     }
+}
+
+export function tests( config: MysqlConfig ): any[] {
+    return [{
+        name: "post_{name,color}_returnsId",
+        context: function() {
+            return new class Mock extends MysqlInterface {
+                public result: string = "";
+                public async query( query: string ) {
+                    this.result = query;
+                }
+            }( config );
+        },
+        input: [{
+            database: "db",
+            table: "fruits",
+            properties: {
+                name: "Apple",
+                color: "Green"
+            }
+        }],
+        function: MysqlInterface.prototype.post,
+        expected: "insert into db.fruits ( name, color ) values( \"Apple\", \"Green\" );",
+        output: function() {
+            let mock: any = this;
+            return mock.result;
+        },
+        debug: false,
+        run: true
+    }, {
+        name: "delete_id_returnsVoid",
+        context: function() {
+            return new class mock extends MysqlInterface {
+                public result: string = "";
+                public async query( query: string ) {
+                    this.result = query;
+                }
+            }( config );
+        },
+        input: [{
+            database: "db",
+            table: "fruits",
+            id: {
+                fruitsId: 1
+            }
+        }],
+        function: MysqlInterface.prototype.delete,
+        expected: "delete from db.fruits where fruitsId = 1;",
+        output: function() {
+            let mock: any = this;
+            return mock.result;
+        },
+        debug: false,
+        run: true
+    }, {
+        name: "put_{db,fruits,id,properties}_returnsVoid",
+        context: function() {
+            return new class mock extends MysqlInterface {
+                public result: string = "";
+                public async query( query: string ) {
+                    this.result = query;
+                }
+            }( config );
+        },
+        input: [{
+            database: "db",
+            table: "fruits",
+            id: {
+                fruitsId: 1
+            },
+            properties: {
+                name: "Pear",
+                color: "Brown"
+            }
+        }],
+        function: MysqlInterface.prototype.put,
+        expected: "update db.fruits set name = \"Pear\", color = \"Brown\" where fruitsId = 1;",
+        output: function() {
+            let mock: any = this;
+            return mock.result;
+        },
+        debug: false,
+        run: true
+    }, {
+        name: "get_{db,fruits}_returnsAll",
+        context: function() {
+            return new class mock extends MysqlInterface {
+                public result = "";
+                public async query( query: string ) {
+                    this.result = query;
+                }
+            }( config );
+        },
+        input: [{
+            database: "db",
+            table: "fruits"
+        }],
+        function: MysqlInterface.prototype.get,
+        expected: "select * from db.fruits;",
+        output: function() {
+            let mock: any = this;
+            return mock.result;
+        },
+        debug: false,
+        run: true
+    }, {
+        name: "get_{db,fruits,id}_returnsRow",
+        context: function() {
+            return new class mock extends MysqlInterface {
+                public result = "";
+                public async query( query: string ) {
+                    this.result = query;
+                }
+            }( config );
+        },
+        input: [{
+            database: "db",
+            table: "fruits",
+            id: {
+                fruitsId: 1
+            }
+        }],
+        function: MysqlInterface.prototype.get,
+        expected: "select * from db.fruits where fruitsId = 1;",
+        output: function() {
+            let mock: any = this;
+            return mock.result;
+        },
+        debug: false,
+        run: true
+    }, {
+        name: "get_{db,fruits,emptyWhereObject}_returnsAll",
+        context: function() {
+            return new MysqlInterface( config );
+        },
+        input: [{
+            database: "db",
+            table: "fruits"
+        }],
+        function: async function( input: any ) {
+            let self = this;
+            await self.connect();
+            let all = await self.get({
+                database: input.database,
+                table: input.table
+            });
+            let result = await MysqlInterface.prototype.get.call( self, input );
+            result = JSON.stringify(result);
+            let expected = JSON.stringify(all);
+            let passes = result == expected;
+            await self.close();
+            return passes;
+        },
+        output: true,
+        debug: false,
+        run: false
+    }];
 }
